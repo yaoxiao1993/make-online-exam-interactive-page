4514f5b62e7fd7d74e4df9c28cf37f9272a05af3
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-05-07 22:20:32 +0800

    finished test1, and test from browser

--- main/main.js
+++ main/main.js
@@ -1,6 +1,53 @@
 function main(){
-    return 'hello';
+    var score = 0;
+    if(document.getElementById("1.1").innerHTML === '统一建模语言'){
+        score = score + 5;
+    }
+
+    if(document.getElementById("1.2.1").innerHTML === '封装性'){
+        score = score + 5;
+    }
+
+    if(document.getElementById("1.2.2").innerHTML === '继承性'){
+        score = score + 5;
+    }
+
+    if(document.getElementById("1.2.3").innerHTML === '多态性'){
+        score = score + 5;
+    }
+
+    if(document.getElementById("2.1.2").checked){
+        score = score + 10;
+    }
+
+    if(document.getElementById("2.2.1").checked){
+        score = score + 10;
+    }
+
+    if(document.getElementById("3.1.1").checked && document.getElementById("3.1.2").checked && document.getElementById("3.1.4").checked){
+        score = score + 10;
+    }
+
+    if(document.getElementById("3.2.1").checked && document.getElementById("3.2.2").checked && document.getElementById("3.2.3").checked){
+        score = score + 10;
+    }
+
+    if(document.getElementById("4.1.2").checked){
+        score = score + 10;
+    }
+
+    if(document.getElementById("4.2.1").checked){
+        score = score + 10;
+    }
+
+    if(document.getElementById("5.1").innerHTML === '模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体；可以是某种图形；或者是一种数学表达式。'){
+        score = score + 20;
+    }
+
+    document.getElementById("score").innerHTML = score;
+
+    return score;
 
 }
 
-exports.module = main;
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,10 +1,14 @@
 let fs = require("fs");
-const main = require('../main/main');
+const main = require('../main/main.js');
 
-describe('main()', () => {
-
-    it('should pass', () => {
+describe('main()', () =>{
+    it('should pass', () =>{
       expect(fs.statSync("index.html").isFile()).toBe(true);
     });
+    
+    // var result = main();
+    // it('score', () =>{
+    //   expect(result).toEqual(100);
+    // });
 
 });
